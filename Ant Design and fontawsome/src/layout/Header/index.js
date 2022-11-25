fea046b4623c21f9ecbfe32ef8bc15cc5f254eee
Syed Areeb <sareeb65@gmail.com>
2022-11-25 11:39:48 +0500

    Navbar responsive

--- Ant Design and fontawsome/src/layout/Header/index.js
+++ Ant Design and fontawsome/src/layout/Header/index.js
@@ -1,26 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "../../App.css"
-import { Breadcrumb, Layout, Menu, Anchor } from 'antd';
+import { Breadcrumb, Layout, Menu, Anchor, Button, Drawer } from 'antd';
 
 const { Header, Content, Footer } = Layout;
 const { Link } = Anchor;
-const AppHeader = () => (
-    <div className='container-fluid'>
-        <div className='header'>
-            <div className="logo" >
-                <i className='fas fa-bolt'></i>
-                <a style={{ textDecoration: 'none' }} href='https://www.google.com/'>Tech</a>
+const AppHeader = () => {
+    const [open, setOpen] = useState(false);
+    const showDrawer = () => {
+        setOpen(true);
+    };
+    const onClose = () => {
+        setOpen(false);
+    };
+    return (
+        <div className='container-fluid'>
+            <div className='header'>
+                <div className="logo" >
+                    <i className='fas fa-bolt'></i>
+                    <a style={{ textDecoration: 'none' }} href='https://www.google.com/'>Tech</a>
+                </div>
+                <div className='mobileHidden'>
+                    <Anchor targetOffset='65'>
+                        <Link href="#hero" title="Home" />
+                        <Link href="#about" title="About" />
+                        <Link href="#feature" title="Features" />
+                        <Link href="#works" title="How it works" />
+                        <Link href="#faq" title="FAQ" />
+                        <Link href="#pricing" title="Pricing" />
+                        <Link href="#contact" title="Contact" />
+                    </Anchor>
+                </div>
+                <div className='mobileVisible'>
+                    <Button type="primary">
+                        <i onClick={showDrawer} className="fa-solid fa-bars"></i>
+                    </Button>
+                    <Drawer title="Tech navbar" placement="right" onClose={onClose} open={open}>
+                        <Anchor targetOffset='65'>
+                            <Link href="#hero" title="Home" />
+                            <Link href="#about" title="About" />
+                            <Link href="#feature" title="Features" />
+                            <Link href="#works" title="How it works" />
+                            <Link href="#faq" title="FAQ" />
+                            <Link href="#pricing" title="Pricing" />
+                            <Link href="#contact" title="Contact" />
+                        </Anchor>
+                    </Drawer>
+                </div>
             </div>
-            <Anchor targetOffset='65'>
-                <Link href="#hero" title="Home" />
-                <Link href="#about" title="About" />
-                <Link href="#feature" title="Features" />
-                <Link href="#works" title="How it works" />
-                <Link href="#faq" title="FAQ" />
-                <Link href="#pricing" title="Pricing" />
-                <Link href="#contact" title="Contact" />
-            </Anchor>
         </div>
-    </div>
-);
+    )
+}
 export default AppHeader;
